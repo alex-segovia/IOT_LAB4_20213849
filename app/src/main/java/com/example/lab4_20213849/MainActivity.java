96fbe7389758b8e1b471d4b964e58e1fff8042d9
alex-segovia <a20213849@pucp.edu.pe>
2024-10-04 01:43:17 -0500

    Main activity listo

---
+++ app/src/main/java/com/example/lab4_20213849/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.lab4_20213849;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        verificarConexionAInternet();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        verificarConexionAInternet();
+    }
+
+    public void verificarConexionAInternet(){
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = manager.getActiveNetworkInfo();
+        if(netInfo!=null && netInfo.isConnected()){
+
+        }else{
+            MaterialAlertDialogBuilder dialogInicio = new MaterialAlertDialogBuilder(MainActivity.this);
+            dialogInicio.setTitle("¡Hay un problema!");
+            dialogInicio.setMessage("No tienes conexión a internet :(");
+            dialogInicio.setPositiveButton("Ir a configuración", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(Settings.ACTION_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            dialogInicio.show();
+        }
+    }
+}
